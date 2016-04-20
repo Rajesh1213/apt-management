9ca208e6c5111da07e60850e0a6f9c61de2d727e
Aileen Tran <lienlaiho@gmail.com>
2016-04-20 09:38:02 -0500

    Basic unit testing.

--- test/models/staff_test.rb
+++ test/models/staff_test.rb
@@ -4,4 +4,9 @@ class StaffTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test "should not save staff without information" do
+    staff = Staff.new
+    assert_not staff.save
+  end
 end
